d2028f1ffa1b00d09c3fa7bc0cf5664078495371
FernandoMarquez2020 <86694341+FernandoMarquez2020@users.noreply.github.com>
2021-09-29 12:45:23 -0300

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuLateralComponent } from './menus/menu-lateral/menu-lateral.component';
+import { MenuFooterComponent } from './menus/menu-footer/menu-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLateralComponent,
+    MenuFooterComponent
   ],
   imports: [
     BrowserModule,
